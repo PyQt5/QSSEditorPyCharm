4b5b357d77ef97ad39a9b624e607a76e55bfe65e
Irony <892768447@qq.com>
2024-05-14 18:32:03 +0800

    ✨ feat(config): 增加翻译和配置页面

---
+++ src/main/kotlin/irony/pycharm/qsseditor/LangBundle.kt
@@ -0,0 +1,35 @@
+package irony.pycharm.qsseditor
+
+import com.intellij.DynamicBundle
+import org.jetbrains.annotations.Nls
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.Nullable
+import org.jetbrains.annotations.PropertyKey
+import java.util.function.Supplier
+
+object LangBundle {
+    private const val BUNDLE: @NonNls String = "messages.LangBundle"
+    private val INSTANCE = DynamicBundle(LangBundle::class.java, BUNDLE)
+
+    fun message(
+        key: @PropertyKey(resourceBundle = BUNDLE) String,
+        vararg params: Any
+    ): @Nls String {
+        return INSTANCE.messageOrDefault(key, key, *params)
+    }
+
+    fun message(
+        key: @PropertyKey(resourceBundle = BUNDLE) String,
+        @Nullable @Nls defaultValue: String,
+        vararg params: Any
+    ): @Nls String {
+        return INSTANCE.messageOrDefault(key, defaultValue, *params)
+    }
+
+    fun lazyMessage(
+        @PropertyKey(resourceBundle = BUNDLE) key: String,
+        vararg params: Any
+    ): Supplier<@Nls String> {
+        return INSTANCE.getLazyMessage(key, *params)
+    }
+}
--- src/main/kotlin/irony/pycharm/qsseditor/SettingsConfigurable.kt
+++ src/main/kotlin/irony/pycharm/qsseditor/SettingsConfigurable.kt
@@ -2,15 +2,28 @@ package irony.pycharm.qsseditor
 
 import com.intellij.openapi.options.BoundSearchableConfigurable
 import com.intellij.openapi.options.Configurable.NoScroll
+import com.intellij.openapi.ui.DialogPanel
+import com.intellij.ui.dsl.builder.bindIntText
+import com.intellij.ui.dsl.builder.bindSelected
+import com.intellij.ui.dsl.builder.bindText
 import com.intellij.ui.dsl.builder.panel
 
 class SettingsConfigurable : BoundSearchableConfigurable(
-    "QSS",
-    "QSS Editor Configurable"
+    "QSS Editor",
+    LangBundle.message("setting.topic.title", "QSS Editor Configurable")
 ), NoScroll {
 
-    override fun createPanel() =  panel {
-        row {
+    override fun createPanel() : DialogPanel {
+        return panel {
+            row(LangBundle.message("setting.host.title", "Host")) {
+                textField().bindText(SettingsState.instance::host)
+            }
+            row(LangBundle.message("setting.port.title", "Port")) {
+                textField().bindIntText(SettingsState.instance::port)
+            }
+            row(LangBundle.message("setting.auto.title","Auto Apply")) {
+                checkBox("").bindSelected(SettingsState.instance::auto)
+            }
         }
     }
 }
