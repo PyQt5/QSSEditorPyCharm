4c07b23601e7879923caabdfb5ba5fe17396130c
Irony <892768447@qq.com>
2024-06-05 00:34:56 +0800

    :fire: 增加自动提示限制

--- src/main/kotlin/irony/ide/plugin/qsseditor/QSSCompletion.kt
+++ src/main/kotlin/irony/ide/plugin/qsseditor/QSSCompletion.kt
@@ -15,8 +15,8 @@ package irony.ide.plugin.qsseditor
 import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.util.io.FileUtilRt
 import com.intellij.patterns.PlatformPatterns.psiElement
-import com.intellij.psi.xml.XmlAttributeValue
 import com.intellij.util.ProcessingContext
 
 private val Log = logger<QSSCompletion>()
@@ -25,18 +25,34 @@ class QSSCompletion : CompletionContributor() {
     init {
         extend(
             CompletionType.BASIC,
-            psiElement(XmlAttributeValue::class.java),
+            psiElement(),
             object : CompletionProvider<CompletionParameters>() {
                 override fun addCompletions(
                     parameters: CompletionParameters,
                     context: ProcessingContext,
                     result: CompletionResultSet,
                 ) {
+                    if (!isSupport(parameters)) {
+                        return
+                    }
+
                     Log.info("addCompletions")
                     result.addElement(LookupElementBuilder.create("QPushButton"))
                     result.addElement(LookupElementBuilder.create("QLabel"))
                 }
             },
         )
+    }
+
+    private fun isSupport(parameters: CompletionParameters): Boolean {
+        val file = parameters.originalFile
+        val extension: CharSequence = FileUtilRt.getExtension(file.name, "")
+        val ext: String =
+            if (extension == "") {
+                file.fileType.name.lowercase()
+            } else {
+                extension.toString()
+            }
+        return ext == "qss" || ext == "css" || ext == "style"
     }
 }
